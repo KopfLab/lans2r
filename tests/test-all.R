6c322eb12169b474fc38316bcd8f9e4183242107
Sebastian Kopf <seb.kopf@gmail.com>
2016-01-20 15:04:59 -0500

    test infractrusture added
    no tests yet

---
+++ tests/test-all.R
@@ -0,0 +1,3 @@
+library(testthat)
+library(lans2r)
+test_check("lans2r")
---
+++ tests/testthat/test-01.R
@@ -0,0 +1,5 @@
+context("lans2r testing")
+
+test_that("Testing general functionality - sorry, no unit tests implemented yet", {
+  expect_true(TRUE)
+})
