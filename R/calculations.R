5b443787cf19501b2d318d56555c06c1977a277f
Sebastian Kopf <seb.kopf@gmail.com>
2016-02-09 21:50:50 -0700

    implemented calculate_sums

--- R/calculations.R
+++ R/calculations.R
@@ -1,3 +1,82 @@
+#' Calculate ion sums
+#' 
+#' This function calculates the ion sums and resulting counting
+#' statistics error from multiple raw ion counts. It can be applied to data from both
+#' LANS_summary and LANS_maps loading but can be slow if LANS_maps is combined
+#' from many analyses.
+#' 
+#' @param data a data frame with raw ion counts retrieved from \code{\link{load_analysis_data()}}
+#' @param ... the ion sums to calculate, each entry is for one sum of two ions,
+#' e.g. c("13C", "12C"), c("15N12C", "14C12C"), ...
+#' @param quiet whether the function should output information messages or be quiet (default is to output)
+#' @note TODO: right now only supports sum of two ions, make this more flexible by introducing
+#'  a flexible function call that can take multiple paramters
+#' @return the original data frame with the fractional abundance information appended (all fractoinal abundances are in % and have data_type == "abundance")
+#' @export
+calculate_sums <- function(data, ..., quiet = F) {
+  # checks
+  if(is.null(data$variable)) stop("'variable' column does not exist")
+  if(is.null(data$value)) stop("'value' column does not exist")
+  if(is.null(data$data_type)) stop("'data_type' column does not exist")
+  
+  # checks
+  params <- list(...)
+  missing <- setdiff(params %>% unlist(), filter(data, data_type == "ion_count")$variable %>% unique)
+  if (length(missing) > 0) {
+    stop("some variables do not exist in this data set: ", missing %>% paste(collapse = ", ")) 
+  }
+  
+  ion_sum <- function(a,b) a+b
+  ion_sum_error <- function(a,b) lans2r:::iso.errN(a+b) 
+  
+  # generate function calls to make calculations
+  var_new <- sapply(params, function(i) paste0(i[1],"+",i[2]))
+  val_fields <- 
+    lapply(params, function(i) lazyeval::interp(~ion_sum(NM, Nm), NM = as.name(i[2]), Nm = as.name(i[1]))) %>% 
+    setNames(var_new)
+  err_fields <- 
+    lapply(params, function(i) lazyeval::interp(~ion_sum_error(NM, Nm), NM = as.name(i[2]), Nm = as.name(i[1]))) %>% 
+    setNames(var_new)
+  
+  # existing variables
+  var_old <- (data %>% filter(data_type == "ion_count"))$variable %>% unique() %>% setdiff(var_new)
+  var_old_deselect <- lapply(var_old, function(i) lazyeval::interp(~-var, var = as.name(i)))
+  
+  values <- 
+    data %>% filter(data_type == "ion_count") %>% 
+    select(-sigma) %>% 
+    tidyr::spread(variable, value) %>% 
+    mutate_(.dots = val_fields) %>% 
+    select_(.dots = var_old_deselect) %>% 
+    tidyr::gather_("variable", "value", var_new) 
+  error <- 
+    data %>% filter(data_type == "ion_count") %>% 
+    select(-sigma) %>% 
+    tidyr::spread(variable, value) %>% 
+    mutate_(.dots = err_fields) %>% 
+    select_(.dots = var_old_deselect) %>% 
+    tidyr::gather_("variable", "sigma", var_new) 
+  
+  if (!quiet) {
+    sprintf(
+      paste0(
+        "INFO: %d isotope sums + errors calculated and added to the data frame with data_type 'ion_count'",
+        "\n      isotopes sums added (stored in 'variable' column): %s"),
+      nrow(values), 
+      (values %>% count(variable) %>% mutate(label = paste0("'", variable, "' (", n, "x)")))$label %>% paste(collapse = ", ")
+    ) %>% message()
+  }
+  
+  # combine old data with new data
+  bind_rows(
+    data %>% filter(! (data_type == "ion_count" & variable %in% var_new)), # make sure no duplicates
+    suppressMessages(left_join(values, error)) %>% 
+      filter(!is.na(value)) %>% # remove non existent abundances
+      mutate(data_type = "ion_count") %>% 
+      mutate(variable = as.character(variable)) # don't like the factor it introduces
+  )
+}
+
 #' Calculate isotope ratios
 #' 
 #' This function calculates the isotope ratios and resulting counting
