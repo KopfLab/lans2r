9ebcc14a456b1593ee6f4d0fb90618459761399c
Sebastian Kopf <seb.kopf@gmail.com>
2016-02-13 17:09:36 -0500

    implement generic calculate

--- R/calculations.R
+++ R/calculations.R
@@ -1,3 +1,88 @@
+#' Calculate derived data
+#' 
+#' This function allows easy calculation of any quantities derived from other variables. The new quantities can be assigned to a specific data_type and values, errors as well as the resulting variable names are calculated/constructed based on custom functions that can be provided via the function parameters. \link{calculate_sums}, \link{calculate_ratios} and \link{calculate abundances} are all based on this and provide an easy way for common standard calculations.
+#' 
+#' @param data a data frame with raw ion counts retrieved from \code{\link{load_analysis_data()}}
+#' @param ... the columns to send to the value, error and naming function for each derived value, e.g. c("12C", "13C", "12C14N"), the number of parameters needs to match those expected by the value, error and name functions
+#' @param value_fun a custom function used to calculate the derived value - needs to match the sets of paramters provided through ...
+#' @param error_fun a custom function used to calcluate the error (sigma) for the derived value
+#' @param name_fun a custom function used to construct the variable name for the derived quantity
+#' @param quiet whether the function should output information messages or be quiet (default is to output)
+#' @return the original data frame with the sums information appended (data_type == "ion_sum")
+#' @export
+calculate <- function(data, data_type, ..., value_fun, 
+                      error_fun = function(...) return(NA), 
+                      name_fun = function(...) return(paste(list(...), collapse = " ")),
+                      quiet = F) {
+  # checks
+  if(is.null(data$variable)) stop("'variable' column does not exist")
+  if(is.null(data$value)) stop("'value' column does not exist")
+  if(is.null(data$data_type)) stop("'data_type' column does not exist")
+  
+  # checks
+  params <- list(...)
+  missing <- setdiff(params %>% unlist(), data$variable %>% unique)
+  if (length(missing) > 0) {
+    stop("some variables do not exist in this data set: ", missing %>% paste(collapse = ", ")) 
+  }
+  
+  # generate function calls to make calculations
+  var_new <- sapply(params, function(i) do.call(name_fun, i %>% as.list()))
+  val_fields <-
+    lapply(params, function(i) {
+      func_call <- sprintf("f(`%s`)", i %>% paste(collapse = "`,`"))
+      as.lazy(func_call, parent.frame()) %>% interp(f = value_fun)
+    }) %>% setNames(var_new)
+  
+  err_fields <-
+    lapply(params, function(i) {
+      func_call <- sprintf("f(`%s`)", i %>% paste(collapse = "`,`"))
+      as.lazy(func_call, parent.frame()) %>% interp(f = error_fun)
+    }) %>% setNames(var_new)
+  
+  # figure out what are the actual new variables (includes overriding old ones)
+  var_old <- data$variable %>% unique() %>% setdiff(var_new)
+  var_new_select <- lapply(var_old, function(i) lazyeval::interp(~-var, var = as.name(i)))
+  
+  values <- 
+    data %>%  
+    select(-sigma, -data_type) %>% 
+    tidyr::spread(variable, value) %>% 
+    mutate_(.dots = val_fields) %>% 
+    select_(.dots = var_new_select) %>% 
+    tidyr::gather_("variable", "value", var_new) 
+  
+  error <- 
+    data %>% 
+    select(-sigma, -data_type) %>% 
+    tidyr::spread(variable, value) %>% 
+    mutate_(.dots = err_fields) %>% 
+    select_(.dots = var_new_select) %>% 
+    tidyr::gather_("variable", "sigma", var_new) 
+  
+  if (!quiet) {
+    sprintf(
+      paste0(
+        "INFO: %d '%s' values + errors calculated and added to the data frame",
+        "\n      values added (stored in 'variable' column): %s"),
+      nrow(values), data_type,
+      (values %>% count(variable) %>% mutate(label = paste0("'", variable, "' (", n, "x)")))$label %>% paste(collapse = ", ")
+    ) %>% message()
+  }
+  
+  # combine old data with new data
+  new_data_type <- data_type
+  bind_rows(
+    data %>% filter(!variable %in% var_new), # make sure no duplicates
+    suppressMessages(left_join(values, error)) %>% 
+      filter(!is.na(value)) %>% # remove calcluations that don't exist
+      mutate(data_type = new_data_type) %>% 
+      mutate(variable = as.character(variable)) # don't like the factor it introduces
+  )
+}
+
+
+
 #' Calculate ion sums
 #' 
 #' This function calculates the ion sums and resulting counting
@@ -6,74 +91,32 @@
 #' from many analyses.
 #' 
 #' @param data a data frame with raw ion counts retrieved from \code{\link{load_analysis_data()}}
-#' @param ... the ion sums to calculate, each entry is for one sum of two ions,
+#' @param ... the ion sums to calculate, each entry is for one sum of as many ions as desired,
 #' e.g. c("13C", "12C"), c("15N12C", "14C12C"), ...
 #' @param quiet whether the function should output information messages or be quiet (default is to output)
-#' @note TODO: right now only supports sum of two ions, make this more flexible by introducing
-#'  a flexible function call that can take multiple paramters
-#' @return the original data frame with the fractional abundance information appended (all fractoinal abundances are in % and have data_type == "abundance")
+#' @return the original data frame with the sums information appended (data_type == "ion_sum")
 #' @export
 calculate_sums <- function(data, ..., quiet = F) {
-  # checks
-  if(is.null(data$variable)) stop("'variable' column does not exist")
-  if(is.null(data$value)) stop("'value' column does not exist")
-  if(is.null(data$data_type)) stop("'data_type' column does not exist")
   
-  # checks
-  params <- list(...)
-  missing <- setdiff(params %>% unlist(), filter(data, data_type == "ion_count")$variable %>% unique)
-  if (length(missing) > 0) {
-    stop("some variables do not exist in this data set: ", missing %>% paste(collapse = ", ")) 
-  }
+  # function to sum up arbitrary number of vectors by entry
+  sum_vectors <- 
+    function(...) { 
+      r <- ..1
+      for(i in list(...)[-1]) r <- r+i
+      return(r)
+    }
   
-  ion_sum <- function(a,b) a+b
-  ion_sum_error <- function(a,b) lans2r:::iso.errN(a+b) 
-  
-  # generate function calls to make calculations
-  var_new <- sapply(params, function(i) paste0(i[1],"+",i[2]))
-  val_fields <- 
-    lapply(params, function(i) lazyeval::interp(~ion_sum(NM, Nm), NM = as.name(i[2]), Nm = as.name(i[1]))) %>% 
-    setNames(var_new)
-  err_fields <- 
-    lapply(params, function(i) lazyeval::interp(~ion_sum_error(NM, Nm), NM = as.name(i[2]), Nm = as.name(i[1]))) %>% 
-    setNames(var_new)
-  
-  # existing variables
-  var_old <- (data %>% filter(data_type == "ion_count"))$variable %>% unique() %>% setdiff(var_new)
-  var_old_deselect <- lapply(var_old, function(i) lazyeval::interp(~-var, var = as.name(i)))
-  
-  values <- 
-    data %>% filter(data_type == "ion_count") %>% 
-    select(-sigma) %>% 
-    tidyr::spread(variable, value) %>% 
-    mutate_(.dots = val_fields) %>% 
-    select_(.dots = var_old_deselect) %>% 
-    tidyr::gather_("variable", "value", var_new) 
-  error <- 
-    data %>% filter(data_type == "ion_count") %>% 
-    select(-sigma) %>% 
-    tidyr::spread(variable, value) %>% 
-    mutate_(.dots = err_fields) %>% 
-    select_(.dots = var_old_deselect) %>% 
-    tidyr::gather_("variable", "sigma", var_new) 
-  
-  if (!quiet) {
-    sprintf(
-      paste0(
-        "INFO: %d isotope sums + errors calculated and added to the data frame with data_type 'ion_count'",
-        "\n      isotopes sums added (stored in 'variable' column): %s"),
-      nrow(values), 
-      (values %>% count(variable) %>% mutate(label = paste0("'", variable, "' (", n, "x)")))$label %>% paste(collapse = ", ")
-    ) %>% message()
-  }
-  
-  # combine old data with new data
-  bind_rows(
-    data %>% filter(! (data_type == "ion_count" & variable %in% var_new)), # make sure no duplicates
-    suppressMessages(left_join(values, error)) %>% 
-      filter(!is.na(value)) %>% # remove non existent abundances
-      mutate(data_type = "ion_sum") %>% 
-      mutate(variable = as.character(variable)) # don't like the factor it introduces
+  # calculate sums
+  calculate(
+    data,
+    data_type = "ion_sum",
+    ...,
+    value_fun = sum_vectors,
+    error_fun = function(...) {
+      lans2r:::iso.errN(sum_vectors(...))
+    },
+    name_fun = function(...) paste(list(...), collapse = "+"),
+    quiet = quiet
   )
 }
 
@@ -93,63 +136,16 @@ calculate_sums <- function(data, ..., quiet = F) {
 #' @return the original data frame with the ratio information appended (all ratios have data_type == "ratio")
 #' @export
 calculate_ratios <- function(data, ..., quiet = F) {
-  # checks
-  if(is.null(data$variable)) stop("'variable' column does not exist")
-  if(is.null(data$value)) stop("'value' column does not exist")
-  if(is.null(data$data_type)) stop("'data_type' column does not exist")
   
-  # checks
-  params <- list(...)
-  missing <- setdiff(params %>% unlist(), filter(data, data_type %in% c("ion_count", "ion_sum"))$variable %>% unique)
-  if (length(missing) > 0) {
-    stop("some ion_counts or ion_sums do not exist in this data set: ", missing %>% paste(collapse = ", ")) 
-  }
-  
-  # generate function calls to make calculations
-  var_new <- sapply(params, function(i) paste0(i[1],"/",i[2]))
-  val_fields <- 
-    lapply(params, function(i) lazyeval::interp(~lans2r:::iso.R(NM, Nm), NM = as.name(i[2]), Nm = as.name(i[1]))) %>% 
-    setNames(var_new)
-  err_fields <- 
-    lapply(params, function(i) lazyeval::interp(~lans2r:::iso.errR(NM, Nm), NM = as.name(i[2]), Nm = as.name(i[1]))) %>% 
-    setNames(var_new)
-  
-  # existing variables
-  var_old <- (data %>% filter(data_type %in% c("ion_count", "ion_sum")))$variable %>% unique() %>% setdiff(var_new)
-  var_old_deselect <- lapply(var_old, function(i) lazyeval::interp(~-var, var = as.name(i)))
-  
-  values <- 
-    data %>% filter(data_type %in% c("ion_count", "ion_sum")) %>% 
-    select(-sigma) %>% 
-    tidyr::spread(variable, value) %>% 
-    mutate_(.dots = val_fields) %>% 
-    select_(.dots = var_old_deselect) %>% 
-    tidyr::gather_("variable", "value", var_new) 
-  error <- 
-    data %>% filter(data_type %in% c("ion_count", "ion_sum")) %>% 
-    select(-sigma) %>% 
-    tidyr::spread(variable, value) %>% 
-    mutate_(.dots = err_fields) %>% 
-    select_(.dots = var_old_deselect) %>% 
-    tidyr::gather_("variable", "sigma", var_new) 
-  
-  if (!quiet) {
-    sprintf(
-      paste0(
-        "INFO: %d ratios + errors calculated and added to the data frame with data_type 'ratio'",
-        "\n      ratios added (stored in 'variable' column): %s"),
-      nrow(values), 
-      (values %>% count(variable) %>% mutate(label = paste0("'", variable, "' (", n, "x)")))$label %>% paste(collapse = ", ")
-    ) %>% message()
-  }
-  
-  # combine old data with new data
-  bind_rows(
-    data %>% filter(! (data_type == "ratio" & variable %in% var_new)), # make sure no duplicates
-    suppressMessages(left_join(values, error)) %>% 
-      filter(!is.na(value)) %>% # remove non existent ratios
-      mutate(data_type = "ratio") %>% 
-      mutate(variable = as.character(variable)) # don't like the factor it introduces
+  # calculate ratios
+  calculate(
+    data,
+    data_type = "ratio",
+    ...,
+    value_fun = function(m, M) lans2r:::iso.R(M, m),
+    error_fun = function(m, M) lans2r:::iso.errR(M, m),
+    name_fun = function(m, M) paste0(m,"/x",M),
+    quiet = quiet
   )
 }
 
@@ -169,62 +165,16 @@ calculate_ratios <- function(data, ..., quiet = F) {
 #' @return the original data frame with the fractional abundance information appended (all fractoinal abundances are in % and have data_type == "abundance")
 #' @export
 calculate_abundances <- function(data, ..., quiet = F) {
-  # checks
-  if(is.null(data$variable)) stop("'variable' column does not exist")
-  if(is.null(data$value)) stop("'value' column does not exist")
-  if(is.null(data$data_type)) stop("'data_type' column does not exist")
   
-  # checks
-  params <- list(...)
-  missing <- setdiff(params %>% unlist(), filter(data, data_type == "ion_count")$variable %>% unique)
-  if (length(missing) > 0) {
-    stop("some ion_counts do not exist in this data set: ", missing %>% paste(collapse = ", ")) 
-  }
-  
-  # generate function calls to make calculations
-  var_new <- sapply(params, function(i) paste(i[1], "F"))
-  val_fields <- 
-    lapply(params, function(i) lazyeval::interp(~100*lans2r:::iso.F(NM, Nm), NM = as.name(i[2]), Nm = as.name(i[1]))) %>% 
-    setNames(var_new)
-  err_fields <- 
-    lapply(params, function(i) lazyeval::interp(~100*lans2r:::iso.errF(NM, Nm), NM = as.name(i[2]), Nm = as.name(i[1]))) %>% 
-    setNames(var_new)
-  
-  # existing variables
-  var_old <- (data %>% filter(data_type == "ion_count"))$variable %>% unique() %>% setdiff(var_new)
-  var_old_deselect <- lapply(var_old, function(i) lazyeval::interp(~-var, var = as.name(i)))
-  
-  values <- 
-    data %>% filter(data_type == "ion_count") %>% 
-    select(-sigma) %>% 
-    tidyr::spread(variable, value) %>% 
-    mutate_(.dots = val_fields) %>% 
-    select_(.dots = var_old_deselect) %>% 
-    tidyr::gather_("variable", "value", var_new) 
-  error <- 
-    data %>% filter(data_type == "ion_count") %>% 
-    select(-sigma) %>% 
-    tidyr::spread(variable, value) %>% 
-    mutate_(.dots = err_fields) %>% 
-    select_(.dots = var_old_deselect) %>% 
-    tidyr::gather_("variable", "sigma", var_new) 
-  
-  if (!quiet) {
-    sprintf(
-      paste0(
-        "INFO: %d abundances + errors calculated (in %%!) and added to the data frame with data_type 'abundance'",
-        "\n      fractional abundances added (stored in 'variable' column): %s"),
-      nrow(values), 
-      (values %>% count(variable) %>% mutate(label = paste0("'", variable, "' (", n, "x)")))$label %>% paste(collapse = ", ")
-    ) %>% message()
-  }
-  
-  # combine old data with new data
-  bind_rows(
-    data %>% filter(! (data_type == "abundance" & variable %in% var_new)), # make sure no duplicates
-    suppressMessages(left_join(values, error)) %>% 
-      filter(!is.na(value)) %>% # remove non existent abundances
-      mutate(data_type = "abundance") %>% 
-      mutate(variable = as.character(variable)) # don't like the factor it introduces
+  # calculate ratios
+  calculate(
+    data,
+    data_type = "abundance",
+    ...,
+    value_fun = function(m, M) 100*lans2r:::iso.F(M, m),
+    error_fun = function(m, M) 100*lans2r:::iso.errF(M, m),
+    name_fun = function(m, M) paste(m, "xF"),
+    quiet = quiet
   )
+  
 }
