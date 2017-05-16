d3fa2f5a80170d5be07c211a5e5e613d27a824ca
Sebastian Kopf <sebastian.kopf@colorado.edu>
2017-05-16 15:39:47 -0600

    documentation and namespace updates

--- R/package.R
+++ R/package.R
@@ -1,19 +1,13 @@
-#' @title lans2r package
-#' @description R interface for working with NanoSIMS ROI data defined in Look at NanoSIMS
-#'
-#' To get started, take alook at the tutorial R markdown file.
-#'
-#' @name lans2r
-#' @docType package
-#' @author Sebastian Kopf
-#'
-#' @include isotope_math.R
-#' @include read_data_files.R
-#' @include load_data.R
-#' @include transformations.R
-#' @include calculations.R
-#' @include read_hmr_files.R
-#' @include plotting.R
-#' @importFrom lazyeval lazy lazy_dots lazy_eval interp
+#' @keywords internal
+"_PACKAGE"
+
+#' @import dplyr
+#' @import tidyr
 #' @import ggplot2
+#' @import stringr
+#' @import ggplot2
+#' @importFrom lazyeval lazy lazy_dots lazy_eval interp
 NULL
+
+## quiets concerns of R CMD check about . that appears in pipelines
+utils::globalVariables(c("."))
