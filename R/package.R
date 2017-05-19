e3e76a21e21a70272fef76f68272123b4666495d
Sebastian Kopf <seb.kopf@gmail.com>
2017-05-18 22:18:05 -0600

    package imports updated

--- R/package.R
+++ R/package.R
@@ -4,9 +4,11 @@
 #' @import dplyr
 #' @import tidyr
 #' @import ggplot2
-#' @import stringr
-#' @import ggplot2
 #' @importFrom lazyeval lazy lazy_dots lazy_eval interp
+#' @importFrom stats setNames sigma var
+#' @importFrom utils read.table
+#' @importFrom R.matlab readMat
+#' @importFrom reshape2 melt
 NULL
 
 ## quiets concerns of R CMD check about . that appears in pipelines
