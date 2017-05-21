ed22ecfdc936c63f346aa3753ffcb7a52e6065c1
Sebastian Kopf <seb.kopf@gmail.com>
2017-05-21 13:01:59 -0600

    add release questions

--- R/package.R
+++ R/package.R
@@ -11,7 +11,15 @@
 #' @importFrom reshape2 melt
 NULL
 
-## quiets concerns of R CMD check about . that appears in pipelines
-## and the standard column names used in non-standard evaluation
+# quiets concerns of R CMD check about . that appears in pipelines
+# and the standard column names used in non-standard evaluation
 utils::globalVariables(c(".", "variable", "value", "analysis", 
                          "x.um", "y.um", "ROI", "plane", "data_type"))
+
+
+# release questions 
+release_questions <- function() {
+  c(
+    "Is it passing travis, appveyor and win-builder?"
+  )
+}
